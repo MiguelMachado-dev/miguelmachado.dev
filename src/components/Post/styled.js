e0f1add37e9abbf5b04a7522023201f959683f31
Miguel Machado <hello@miguelmachado.dev>
2022-04-12 09:45:25 -0300

    feat(tags): change tag style and colors

--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -4,15 +4,15 @@ import media from 'styled-media-query'
 import transitions from 'styles/transitions'
 
 export const PostWrapper = styled.section`
-  align-items: center;
   display: flex;
   padding: 2rem 3rem;
   width: 100%;
   transition: ${transitions.ALL};
 
+  flex-direction: column;
+  align-items: flex-start;
+
   ${media.lessThan('large')`
-    align-items: flex-start;
-    flex-direction: column;
     padding: 2rem 1rem;
   `}
 `
@@ -31,23 +31,21 @@ export const PostLink = styled.a`
 export const PostTag = styled.div`
   align-items: center;
   background: var(--highlight);
-  border-radius: 50%;
   color: var(--white);
   display: flex;
   font-size: 1.3rem;
   font-weight: 700;
   justify-content: center;
-  min-height: 90px;
-  min-width: 90px;
   text-transform: uppercase;
 
+  border-radius: 0.3rem;
+  min-height: auto;
+  min-width: auto;
+  padding: .2rem .5rem;
+  margin-bottom: .7rem;
+
   ${media.lessThan('large')`
-    border-radius: 0;
     font-size: 1rem;
-    min-height: auto;
-    min-width: auto;
-    padding: .2rem .5rem;
-    margin-bottom: .7rem;
   `}
 
   &.is-js {
@@ -56,34 +54,25 @@ export const PostTag = styled.div`
   }
 
   &.is-misc {
-    background: #47650b;
+    background: #7AAB13;
   }
 
   &.is-dev {
-    background: #61728f;
-  }
-
-  &.is-svg {
-    background: #7d669e;
+    background: #637a91;
   }
 
   &.is-css {
-    background: #24809e;
+    background: #2DA0C3;
   }
 
-  &.is-jekyll {
-    background: #b31917;
+  &.is-a11y {
+    background: #009e66;
   }
 `
 
 export const PostInfo = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left: 1.5rem;
-
-  ${media.lessThan('large')`
-    margin: 0;
-  `}
 `
 
 export const PostDate = styled.time`
