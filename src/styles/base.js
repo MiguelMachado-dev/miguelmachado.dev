3c51f766c53f208284effa8cc5ef06427d18e92a
Miguel Machado <hello@miguelmachado.dev>
2025-04-11 15:23:12 -0300

    feat(styles): update code styling for improved readability and consistency

--- src/styles/base.js
+++ src/styles/base.js
@@ -133,13 +133,15 @@ export const MainContent = styled.section`
   }
 
   code {
-    background: #3d375e7f;
+    background: #011627;
     color: white;
     padding: 0.1em;
     border-radius: 0.3em;
     white-space: normal;
     overflow-wrap: break-word;
-    font-family: 'MonoLisa-Regular', Consolas, Monaco, Andale Mono, Ubuntu Mono, monospace;
+    font-family: 'MonoLisa-Regular', 'Fira Code', 'JetBrains Mono', Consolas,
+      Monaco, Andale Mono, Ubuntu Mono, monospace;
+    font-size: 1rem;
   }
 
   pre > code {
